c9b1102551ea6e0b8ce70d4d013d6fafcec9966f
Al- Amin <alamin496832@gmail.com>
2023-12-13 12:37:10 +0600

    refactoring

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/',(req, res)=>{
     res.status(200).json({message:'Hello task manager'})
 })
 app.use(notFound);
-app.use(errorHandler);
+// app.use(errorHandler);
 
 app.listen(port, ()=>{
     console.log('server running on 8000')
--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -2,9 +2,11 @@ const { CustomAPIError } = require("../errors/curstom-error");
 
 
 const errorHandlerMiddleware = (err, req, res, next) => {
+    console.error(err); // Log the error
     if(err instanceof CustomAPIError) {
         return res.status(err.statusCode).json({msg: err.message});
     }
+    console.error(err)
     return res.status(500).json({msg: 'Something went wrong, please try again', err});
 }
 
